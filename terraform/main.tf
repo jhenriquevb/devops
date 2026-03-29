8ced1ae195b3b8dcbb2f3d5cf905411fd29aadda
José Henrique <jhenriquevb11@gmail.com>
2026-03-29 16:06:23 -0300

    feat: adicionar key pair na EC2

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,6 +104,7 @@ resource "aws_instance" "app" {
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.app.id]
+  key_name               = "devops-key"
 
   user_data = <<-EOF
     #!/bin/bash
